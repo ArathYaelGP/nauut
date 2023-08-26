0c50a4b4e80e178c8f1b77fe11aff99bc7576060
ArathG <139999393+ArathYaelGP@users.noreply.github.com>
2023-08-26 17:11:42 -0600

    Projects

---
+++ shared-components/clients.js
@@ -0,0 +1,28 @@
+const clientsCode =
+'       <div class="container">\n' +
+'                <div class="row">\n' +
+'                    <div class="col-md-12 owl-carousel owl-theme">\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/1.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/2.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/3.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/4.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/5.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                        <div class="client-logo">\n' +
+'                            <a href="#"><img src="assets/img/clients/6.png" alt=""></a>\n' +
+'                        </div>\n' +
+'                    </div>\n' +
+'                </div>\n' +
+'            </div>';
+
+const clientsContainer = document.getElementById('clients');
+clientsContainer.innerHTML = clientsCode;
---
+++ shared-components/testimonials.js
@@ -0,0 +1,26 @@
+const testimonialsCode =
+'           <div class="container">\n' +
+'                <div class="row">\n' +
+'                    <div class="col-md-6 offset-md-3">\n' +
+'                        <div class="testimonials">\n' +
+'                            <div class="owl-carousel owl-theme text-center">\n' +
+'                                <div class="item">\n' +
+'                                    <p class="mb-4">" Vivamus faucibus lorem non mi varius, vel euismod magna malesuada. Nam nec diam in nisl porttitor commodo in et ligula. "</p>\n' +
+'                                    <div class="client-area">\n' +
+'                                        <h6>Emma Brown</h6> <span>Claire Towers, CEO</span>\n' +
+'                                    </div>\n' +
+'                                </div>\n' +
+'                                <div class="item">\n' +
+'                                    <p class="mb-4">" Qaulity faucibus lorem non mi varius, vel euismod magna malesuada. Nam nec diam in nisl porttitor commodo in et ligula. "</p>\n' +
+'                                    <div class="client-area">\n' +
+'                                        <h6>Jesica White</h6> <span>Drana Inc, Manager</span>\n' +
+'                                    </div>\n' +
+'                                </div>\n' +
+'                            </div>\n' +
+'                        </div>\n' +
+'                    </div>\n' +
+'                </div>\n' +
+'            </div>';
+
+const testimonialsContainer = document.getElementById('testimonials');
+testimonialsContainer.innerHTML = testimonialsCode;
